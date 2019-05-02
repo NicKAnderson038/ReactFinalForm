683ed8b5d7eca756c3cb144eb615b6befaf37894
Nick Anderson <nickanderson@gmail.com>
2019-05-02 13:57:40 -0400

    .

--- src/FinalForm.js
+++ src/FinalForm.js
@@ -1,19 +1,24 @@
 import React from "react";
 import Input from "./Input";
+import { Form, Field } from "react-final-form";
 
 const FinalForm = () => (
-  <div>
-    <h2>Final Form</h2>
-    <form>
-      <Input
-        type="text"
-        label="Customer ID"
-        id="customer-id"
-        name="customer-id"
-      />
-      <button type="submit">Submit</button>
-    </form>
-  </div>
+  <Form
+    render={() => (
+      <div>
+        <h2>Final Form</h2>
+        <form>
+          <Input
+            type="text"
+            label="Customer ID"
+            id="customer-id"
+            name="customer-id"
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    )}
+  />
 );
 
 export default FinalForm;
